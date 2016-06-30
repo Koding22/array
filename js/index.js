3031670cd1bc9d4c516b653e8923426d1bde27d2
Koding22 <student12@js-c02kw8v9f5n7.punahou.edu>
2016-06-30 13:36:42 -1000

    made it better

--- js/index.js
+++ js/index.js
@@ -1,25 +1,25 @@
-var Ingredients = ["tomatoes", "beef", "lettuce",
- "cheese", "ketchup", "mayonaise", "onions"]
+var Juice = ["normal Peach juice", " warm Gatorade", "Perrier",
+ "calamansi", "pepsi", "unsparkling sparkling water", "lemon tea"]
 
- function pickIngredients(){
- 	//Sets avariable called IngredientsNumber that wil have a random Whole Number
- 	//Based on the length of the Ingredients array
-	var ingredientsNumber = Math.floor(Math.random()*Ingredients.length);
-	//Sets avariable called IngredientsNumber2 that wil have a random Whole Number
- 	//Based on the length of the Ingredients array
- 	var ingredientsNumber2 = Math.floor(Math.random()*Ingredients.length);
- 	//Sets avariable called IngredientsNumber3 that wil have a random Whole Number
- 	//Based on the length of the Ingredients array
- 	var ingredientsNumber3 = Math.floor(Math.random()*Ingredients.length);
+ function pickJuice(){
+ 	//Sets avariable called JuiceNumber that wil have a random Whole Number
+ 	//Based on the length of the Juice array
+	var JuiceNumber = Math.floor(Math.random()*Juice.length);
+	//Sets avariable called JuiceNumber that wil have a random Whole Number
+ 	//Based on the length of the Juice array
+ 	var JuiceNumber2 = Math.floor(Math.random()*Juice.length);
+ 	//Sets avariable called JuiceNumber2 that wil have a random Whole Number
+ 	//Based on the length of the Juice array
+ 	var JuiceNumber3 = Math.floor(Math.random()*Juice.length);
+ 	//Sets avariable called JuiceNumber3 that wil have a random Whole Number
+ 	//Based on the length of the Juice array
 
-	document.getElementById('hamburger').
-	innerHTML = "<h1>" + Ingredients[ingredientsNumber]
-	+" and "+Ingredients[ingredientsNumber2]+"</h1>";
-
-
-
-
-
-
-
+	document.getElementById('Juice').
+	innerHTML = "<h1>" + Juice[JuiceNumber]
+	+" and "+Juice[JuiceNumber2]+"</h1>";
  }
+
+
+
+
+	
